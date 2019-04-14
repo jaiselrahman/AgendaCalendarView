07216bfa9e5deed8d124a959b971e8d6bde3142d
Jaisel Rahman <jaiselrahman@gmail.com>
2019-04-14 21:59:48 +0530

    Add basic agenda view

---
+++ app/src/main/java/com/jaiselrahman/agendacalendar/BaseEvent.java
@@ -0,0 +1,12 @@
+package com.jaiselrahman.agendacalendar;
+
+public interface BaseEvent {
+
+    String getTitle();
+
+    String getDescription();
+
+    String getLocation();
+
+    long getTime();
+}
